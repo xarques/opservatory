997d6dab09d08ba7b638e9645ec071ce0adf9b4b
Xavier Arques <xavierarques@yahoo.com>
2017-12-04 14:16:51 +0100

    Move challenges in db/challenges directory

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,14 @@ end
 puts "#{User.count} users have been successfully created"
 
 puts 'Creating challenges...'
-file = "db/challenges.yml"
-challenges = YAML.load(open(file).read)
 
-challenges["challenges"].each do |challenge|
-  c = Challenge.create!(challenge)
-  puts "  Add Challenge: #{c.name}"
+Dir.foreach("db/challenges") do |file|
+  file_path = "db/challenges/#{file}"
+  if file_path.end_with?(".yml") && File.file?(file_path)
+    challenge = YAML.load(open(file_path).read)
+    c = Challenge.create!(challenge)
+    puts "  Add Challenge #{file}: #{c.name}"
+  end
 end
 
 puts "#{Challenge.count} challenges have been successfully created"
