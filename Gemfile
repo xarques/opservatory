0151c3fedd4b1b7491878b64e4b599ce5b5538b3
Yan de Catheu <ianpurdom75@gmail.com>
2017-12-04 19:13:56 +0100

    add role and pundit

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'pundit'
 
 group :development, :test do
   gem 'pry-byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,17 @@ require "yaml"
 puts "Cleaning database..."
 Challenge.destroy_all
 User.destroy_all
+Role.destroy_all
+UserRole.destroy_all
+
+puts "Creating roles..."
+file = "db/roles.yml"
+roles = YAML.load(open(file).read)
+
+roles["roles"].each do |role|
+  r = Role.create!(role)
+end
+puts "Roles have been created"
 
 puts 'Creating users...'
 file = "db/users.yml"
@@ -12,9 +23,12 @@ users = YAML.load(open(file).read)
 
 users["users"].each do |user|
   u = User.create!(user)
+  u.roles << Role.find_by(name: "teacher")
+  u.roles << Role.find_by(name: "student")
   puts "  Add User: #{u.email}"
 end
 puts "#{User.count} users have been successfully created"
+
 
 puts 'Creating challenges...'
 
