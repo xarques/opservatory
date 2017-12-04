c1fbe989c9e8b9bd843ba8a8111358183ba8967f
Xavier Arques <xavierarques@yahoo.com>
2017-12-04 09:05:29 +0100

    Add users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,25 @@
 require "open-uri"
 require "yaml"
 
-file = "db/seeds.yml"
-sample = YAML.load(open(file).read)
 puts "Cleaning database..."
 Challenge.destroy_all
+User.destroy_all
+
+puts 'Creating users...'
+file = "db/users.yml"
+users = YAML.load(open(file).read)
+
+users["users"].each do |user|
+  u = User.create!(user)
+  puts "  Add User: #{u.email}"
+end
+puts "#{User.count} users have been successfully created"
 
 puts 'Creating challenges...'
-sample["challenges"].each do |challenge|
+file = "db/challenges.yml"
+challenges = YAML.load(open(file).read)
+
+challenges["challenges"].each do |challenge|
   c = Challenge.create!(challenge)
   puts "  Add Challenge: #{c.name}"
 end
