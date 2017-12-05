1c782ecfee426c365cc341701b85201e22e3f385
Yan de Catheu <ianpurdom75@gmail.com>
2017-12-05 14:54:58 +0100

    update routes with exercises nested into challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :challenges
+  resources :challenges do
+    resources :exercises, only: [:create]
+  end
+
   root to: "challenges#index"  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
