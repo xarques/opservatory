1701a6a9d1622eea3977083a2806c84513cdedf7
Tess Saffar <tess.saf@hotmail.fr>
2017-12-05 18:51:46 +0000

    homepage draft 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :challenges
-  root to: "challenges#index"  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "pages#home"  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
