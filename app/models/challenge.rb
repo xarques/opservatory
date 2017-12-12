f66f63f04e665a02cf306f32993730dfcfd175c2
Xavier Arques <xavierarques@yahoo.com>
2017-12-12 17:50:18 +0100

    Add aceEditor in create challenge page

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -5,6 +5,7 @@ class Challenge < ApplicationRecord
   has_many :hints, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :status, inclusion: {in: [0, 1]} # 0 = Draft, 1 = Release
+  validates :photo, presence: true
 
   include PgSearch
   pg_search_scope :search_by_name_and_description,
